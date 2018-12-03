241e5cc55c7f7f568194a74394a210d3e1f9f3a6
Litao Lu <lulitao1997@gmail.com>
2018-12-03 16:11:50 +0800

    > can output lexer token  type now

--- src/parser_exe.cpp
+++ src/parser_exe.cpp
@@ -12,9 +12,15 @@ int main() {
 
     ParserOutput out;
     auto in = istringstream(parser_input);
-    yy::scanner s;
+    yy::scanner s(&in);
     // while (s.yylex()) ;
-    yy::parser p(s, out);
-    int result = p.parse();
+    int tktype = 0;
+    yy::parser::location_type yylloc;
+    while (tktype = s.yylex(new yy::parser::semantic_type, &yylloc)) {
+        cout << tktype << ", " << yylloc << endl;
+        if (!tktype) break;
+    }
+    // yy::parser p(s, out);
+    // int result = p.parse();
     std::cout << out.result << std::endl;
 }

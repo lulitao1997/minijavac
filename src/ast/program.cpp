ab3a97253a143cd5bbd2c3140bff3346cd1afcaa
Litao Lu <lulitao1997@gmail.com>
2018-12-23 16:21:30 +0800

    type inherent map finished

--- src/ast/program.cpp
+++ src/ast/program.cpp
@@ -3,10 +3,32 @@
 
 namespace ast{
 
-std::unordered_map<std::string, Class*> *Type::M = nullptr;
-
 Class *Type::make_class(std::string id, std::string parent, std::vector<ParamDecl*> attrs, std::vector<Method*> methods) {
     return new Class(id, parent, attrs, methods);
+}
+
+std::unordered_map<std::string, Class*> *Type::M;
+
+Type::Type(std::string id) {
+    using namespace std;
+    if (!M) {
+        M = new unordered_map<string, Class*>;
+
+        auto *p = new unordered_map<string, Class*>{
+            {"int", make_class("int", "<object>", {}, {})},
+            {"boolean", make_class("boolean", "int", {}, {})},
+            {"void", make_class("void", "<object>", {}, {})},
+            {"String", make_class("String", "<object>", {}, {})},
+            {"<object>", make_class("<object>", "<object>", {}, {})}
+        };
+        M = p;
+        // cerr << "new umap" << endl;
+    }
+    c = &(*M)[id];
+    if (*id.rbegin() == ']' && !*c) {
+        *c = make_class(id, string(id.begin(), id.end()-2), {}, {});
+    }
+
 }
 
 }
--- src/ast/program.hpp
+++ src/ast/program.hpp
@@ -13,28 +13,8 @@ struct Method;
 
 struct Class;
 
-Class *make_class(std::string id, std::string parent, std::vector<ParamDecl*> attrs, std::vector<Method*> methods);
 struct Type {
-    explicit Type(std::string id) {
-        using namespace std;
-        if (!M) {
-            M = new unordered_map<string, Class*>;
-
-            auto *p = new unordered_map<string, Class*>{
-                {"int", make_class("int", "<object>", {}, {})},
-                {"boolean", make_class("boolean", "int", {}, {})},
-                {"void", make_class("void", "<object>", {}, {})},
-                {"String", make_class("String", "<object>", {}, {})},
-                {"<object>", make_class("<object>", "<object>", {}, {})}
-            };
-            M = p;
-            cerr << "new umap" << endl;
-        }
-        c = &(*M)[id];
-        if (*id.rbegin() == ']' && !*c) {
-            *c = make_class(id, string(id.begin(), id.end()-2), {}, {});
-        }
-    }
+    explicit Type(std::string id);
 
     Class& operator*() {
         assert(*c);
